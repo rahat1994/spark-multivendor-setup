490c6400200e70df07a666796d087dfbd3505be3
Rahat Baksh <rahat392@gmail.com>
2024-06-16 17:57:49 +0600

    adding "Top Vendor" action to vendor resource table

--- app/Providers/WholesaverVendorServiceProvider.php
+++ app/Providers/WholesaverVendorServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Actions\Action;
+use Filament\Actions\EditAction;
 use Illuminate\Support\ServiceProvider;
 
 use Filament\Forms\Components\Select;
@@ -10,6 +12,9 @@ use Filament\Forms\Components\TextInput;
 
 use Rahat1994\SparkcommerceMultivendor\Filament\Pages\Tenancy\RegisterVendor;
 use Filament\Forms\Form;
+use Filament\Tables\Actions\EditAction as ActionsEditAction;
+use Rahat1994\SparkcommerceMultivendor\Filament\Resources\VendorResource;
+use Rahat1994\SparkcommerceMultivendor\Models\SCMVVendor;
 
 class WholesaverVendorServiceProvider extends ServiceProvider
 {
@@ -54,6 +59,51 @@ class WholesaverVendorServiceProvider extends ServiceProvider
                         ->required()
                         ->placeholder('Postcodes where the vendor delivers'),
                 ]);
+        });
+
+
+        VendorResource::macro('tableActions', function () {
+            $topVendor = Action::make('make_top_vendor')
+                ->label('Make Top Vendor')
+                // ->requiresConfirmation()
+                ->visible(function (SCMVVendor $record) {
+                    return ($record->meta == null || $record->meta['is_top_vendor'] != 1);
+                })
+                ->action(function (SCMVVendor $record) {
+                    // dd($record->meta);
+
+                    if ($record->meta == null) {
+
+                        $record->meta = [
+                            'is_top_vendor' => 1
+                        ];
+
+                        $record->save();
+                    }
+                });
+
+            $demoteVendor = Action::make('demote_vendor')
+                ->label('Demote Vendor')
+                // ->requiresConfirmation()
+                ->visible(function (SCMVVendor $record) {
+                    return ($record->meta != null && $record->meta['is_top_vendor'] == 1);
+                })
+                ->action(function (SCMVVendor $record) {
+                    // dd($record->meta);
+
+                    if ($record->meta == null) {
+                        $record->meta = [
+                            'is_top_vendor' => 0
+                        ];
+                        $record->save();
+                    }
+                });
+
+            return [
+                ActionsEditAction::make(),
+                $topVendor,
+                $demoteVendor
+            ];
         });
     }
 }
